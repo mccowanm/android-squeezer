d3e340ab54bc04a29dc8ad8abb521a4838715f88
Kurt Aaholst <kaaholst@gmail.com>
2021-02-02 23:11:39 +0100

    onRequestPermissionsResult call super

--- Squeezer/src/main/java/uk/org/ngo/squeezer/framework/BaseActivity.java
+++ Squeezer/src/main/java/uk/org/ngo/squeezer/framework/BaseActivity.java
@@ -519,6 +519,7 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
             case 1:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {

81e63a296feffb2f275fec70fd7902588fe46780
Kurt Aaholst <kaaholst@gmail.com>
2021-06-08 11:31:26 +0200

    Pause before toggling "Fixed Volume 100%"

--- Squeezer/src/main/java/uk/org/ngo/squeezer/service/SqueezeService.java
+++ Squeezer/src/main/java/uk/org/ngo/squeezer/service/SqueezeService.java
@@ -1375,6 +1375,7 @@ public class SqueezeService extends Service {
             if (!isConnected()) {
                 return;
             }
+            maybePauseBeforeAction(action.action);
             mDelegate.command(getActivePlayer()).cmd(action.action.cmd).params(action.action.params(item.inputValue)).exec();
         }
 
@@ -1383,7 +1384,18 @@ public class SqueezeService extends Service {
             if (!isConnected()) {
                 return;
             }
+            maybePauseBeforeAction(action);
             mDelegate.command(getActivePlayer()).cmd(action.cmd).params(action.params).exec();
+        }
+
+        private void maybePauseBeforeAction(Action.JsonAction action) {
+            if (isaPotentiallyDangerousAction(action)) {
+                pause();
+            }
+        }
+
+        private boolean isaPotentiallyDangerousAction(Action.JsonAction action) {
+            return Util.arraysStartsWith(action.cmd(), new String[]{"playerpref", "digitalVolumeControl"});
         }
 
         @Override
